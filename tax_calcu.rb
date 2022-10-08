91e9a1cf7919daf87cbd7aa7c2388fa569590e1d
Ubuntu <ubuntu@ip-172-31-44-116.ap-northeast-1.compute.internal>
2022-10-08 01:33:45 +0000

    tax_calcu

--- tax_calcu.rb
+++ tax_calcu.rb
@@ -15,24 +15,24 @@ Aさんの１００万円の所得の内0 ~ 10万円 に対しては、税金が
 =end 
 
 # 所得を変数に代入する
-income = 1000000
+income = 100000
 
 # 税金を代入する変数
 tax = 0
 
 # 税額計算
-while (income < 0){
-if income < 100000{
+while (income < 0)
+if income < 100000
    tax = tax + 0
    income = income - 100000
-}
+end 
+end 
 
-if income < 700000{
+if income < 700000 
 tax = tax + (income - 100000) * 0.1
 income = income - 100000
-}else{
+else
 tax = tax + (income - 700000) * 0.3
 income = income - 700000
-}
-print tax;
-}
+end 
+print tax; 
